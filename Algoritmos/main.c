c02ac9df86f0b9cd3a4893ac17674d0790d6639e
Victor Ciurlino <victorciurlini@gmail.com>
2021-04-17 21:08:42 -0300

    bd v1.0 finalizada

--- Algoritmos/main.c
+++ Algoritmos/main.c
@@ -4,19 +4,23 @@
 #include <time.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <mysql/mysql.h>
+#include <sys/wait.h>
+
 
 // Inicializa as funções.
 char* datetime();
 char* cleandata(char* data);
-void* move_file(char* path, char* data);
+char* move_file(char* path, char* data);
+void* populate_mysql(MYSQL *con, char* path_database, char* file_name, char* data);
+void finish_with_error(MYSQL *con);
 
-int main()
+int main(int argc, char **argv)
 {
     /********Declarando variáveis*******/
     char ch, file_name[100];
     char path[20] = "../Photos/";
-    //char ext[20] = ".txt";
-    char full_path[200] = "";
+    char *path_database;
     char *data = datetime();
     char *nome_data = cleandata(data);
     char *cmd;
@@ -31,7 +35,7 @@ int main()
     DIR *directory;
     FILE *fp;
     clock_t start, stop;
-
+    MYSQL *con = mysql_init(NULL);
     /***********************************/
 
     size = strlen(format)+strlen(folder)+1;
@@ -41,55 +45,61 @@ int main()
     //printf("executing: %s\n", cmd);
 
     status = system(cmd);
-    //exitcode = WEXITSTATUS(status);
+    exitcode = WEXITSTATUS(status);
 
     //printf ("exit code: %d, exit status: %d\n", exitcode, status);
-
     //Verifica se o diretório está vazio, aguarda 5 segundos para verificar novamente
     if (exitcode == 1){
-            sleep(5);
+            //sleep(5);
             printf("O diretório está vazio\n");
     }
-    else
+    else{
+        //Caso o repositório não estiver vazio, ele é aberto
+        //Para receber o conteúdo
         directory = opendir(folder);
 
+        //Ao abrir o repositório, ele lê todos os arquivos contidos
         while((entry=readdir(directory))){
-            files++;
-            char *filename = entry->d_name;
+            files++; // Incrimenta pelos arquivos
+            char *filename = entry->d_name; //Recebe o nome dos arquivos
+            
+            //Loop para ignorar os repositórios "." e ".."
             for(i=0; filename[i]; i++){
                 if (filename[i] == '.'){
                     break;
                 }
                 else{
-                    strcpy(file_name, filename);
+                    strcpy(file_name, filename); //Copia o nome da imagem para outra variável
                 }
             }
-            strcat(path, file_name);
-            compare = strcmp(path, "../Photos/");
+            strcat(path, file_name); //Concatena o nome do arquivo com o path para próxima verificação
+            compare = strcmp(path, "../Photos/"); // Verifica se o repositório é vazio
             if(compare != 0){
                 printf("compare: %d\n", compare);
                 printf("file_name: %s\n", file_name);
                 printf("path: %s\n", path);
                 printf("Nome do arquivo: %s\n", file_name);
                 printf("Caminho do arquivo: %s\n", path);
-                fp = fopen(path, "r");
 
-                if (fp == NULL){
-                    perror("Arquivo não encontrado.\n");
-                    exit(EXIT_FAILURE);
-                }
-                else
-                    printf("Arquivo encontrado!\n");
-                    move_file(path, nome_data);
+                //Abre o arquivo
+                fp = fopen(path, "r");
+                printf("Arquivo encontrado!\n");
+
+                //Move o arquivo para a pasta "database"
+                path_database = move_file(path, nome_data);
+                printf("path_database: %s\n", path_database);
+                
+                populate_mysql(con, path_database, file_name, data);
             }
+            //reinicia a variável path
             strcpy(path, "../Photos/");
         }
         free(cmd);
         closedir(directory);
-       
+    }
+
     return 0;
 }
-
 
 //Retorna o horário em que a imagem foi acessada
 char* datetime(){
@@ -112,9 +122,8 @@ char* datetime(){
 
     return timeString;
 }
-
-// Função para limpar o datetime e transformar em nome para
-// figura
+// Função para limpar o datetime e transformar
+// em nome para figura
 char* cleandata(char* data){
     int i,j;
 
@@ -129,7 +138,7 @@ char* cleandata(char* data){
     return data;
 }
 //Move o arquivo encontrado na pasta Photos
-void* move_file(char* path, char* nome_data){
+char* move_file(char* path, char* nome_data){
 
     char* path_database = "../database/";
     char* ext = ".jpg";
@@ -138,4 +147,45 @@ void* move_file(char* path, char* nome_data){
     sprintf(novo_path, "%s%s%s", path_database, nome_data, ext);
     printf("Movendo arquivo para: %s\n", novo_path);
     rename(path, novo_path);
+
+    return novo_path;
+}
+
+void* populate_mysql(MYSQL *con, char* path_database, char* file_name, char* data){
+
+    printf("MySQL client version: %s\n", mysql_get_client_info());
+    
+    printf("passou do mysql con ");
+    char *sql_command;
+    sql_command = malloc(sizeof(char)*100);
+    if (con == NULL){
+        fprintf(stderr, "%s\n", mysql_error(con));
+        exit(1);
+    }
+
+  if (mysql_real_connect(con, 
+                        "localhost", 
+                        "cliente", 
+                        "Cliente@0001",
+                        "DATABASE_PATOGENOS", 0, NULL, 0) == NULL){
+        finish_with_error(con);
+    }
+
+    sprintf(sql_command, "INSERT INTO dado_folha VALUES ('%s', '%s', '%s')", file_name, data, path_database);
+
+    if (mysql_query(con, sql_command)) {
+        finish_with_error(con);
+    }
+
+    // if (mysql_query(con, "INSERT INTO cars VALUES(40,'Mercedes',57127)")) {
+    //     finish_with_error(con);
+    // }
+    //mysql_close(con);
+    exit(0);
+}
+
+void finish_with_error(MYSQL *con){
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);
 }
--- Algoritmos/teste.c
+++ Algoritmos/teste.c
@@ -1,26 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
- 
-int main()
+int main(int argc, char **argv)
 {
-    char s[1000],c;  
-    int i,count=0;
- 
-    printf("Enter  the string : ");
-    gets(s);
-    printf("Enter character to be searched: ");
-    c=getchar();
-    
-    for(i=0;s[i];i++)  
-    {
-    	if(s[i]==c)
-    	{
-          count++;
-		}
- 	}
-     
-	printf("character '%c' occurs %d times \n ",c,count);
- 
- 	 
-     
-    return 0;
+    int num = 2;
+    char *texto = "alooo galera de cowboy";
+    char *insert = "INSERT INTO";
+    char *size_string;
+    size_string = malloc(sizeof(char)*100);
+    //"INSERT INTO cars VALUES(1, 'AudiTT',23642)"
+    sprintf(size_string, "E aqui eu posso até já colocar o texto %s %s", texto, insert);
+    printf("%s", size_string);
+  exit(0);
 }
